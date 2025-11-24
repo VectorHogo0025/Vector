a1a2cd6a370a2d8f2532cc1d59cdb5a14d3aa688
VectorHogo0025 <alidarkness0011@gmail.com>
2025-11-24 19:13:26 +0330

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div style={{ padding: 30 }}>
+      <h1>Sales Tracker Dashboard</h1>
+
+      <div className="card">
+        <h2>Welcome 👋</h2>
+        <p>Your dashboard is now running from GitHub Pages.</p>
+      </div>
+    </div>
+  );
+}
